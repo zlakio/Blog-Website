fe195eee63416f2df6558659ef4d67766a845930
zlakio <qubealpha1102@gmail.com>
2026-08-30 03:12:38 +0530

    made change in font

--- frontend/blog-website/app/layout.js
+++ frontend/blog-website/app/layout.js
@@ -1,33 +1,19 @@
-import Navbar from "@/components/Navbar";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata = {
-  title: "Your Name | Blog",
-  description: "A blog about my projects and learnings",
-  
-};
-
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link
+          href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className="min-h-full flex flex-col bg-neutral-950 text-neutral-100">
-  <Navbar />
-  {children}
-</body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
