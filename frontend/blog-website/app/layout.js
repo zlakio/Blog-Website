5b4bb200238d08a95fa5504fe0a32eab1796ba11
zlakio <qubealpha1102@gmail.com>
2026-08-30 03:22:51 +0530

    made change in font

--- frontend/blog-website/app/layout.js
+++ frontend/blog-website/app/layout.js
@@ -4,12 +4,6 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        <link
-          href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
       <body className="min-h-full flex flex-col bg-neutral-950 text-neutral-100">
         <Navbar />
         {children}
