3afbf23d19a83a1dca82fadb0a2bed07fa946748
zlakio <qubealpha1102@gmail.com>
2026-08-30 03:37:34 +0530

    made change in font

--- frontend/blog-website/app/layout.js
+++ frontend/blog-website/app/layout.js
@@ -1,8 +1,14 @@
+import localFont from "next/font/local";
+
+const cabinetGrotesk = localFont({
+  src: "../fonts/CabinetGrotesk-Bold.woff",
+  variable: "--font-cabinet",
+});
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistSans.variable} ${geistMono.variable} ${cabinetGrotesk.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-neutral-950 text-neutral-100">
         <Navbar />
